9c239f258179720228a2c378fd8263a1bab113ee
Tudor Tacal <tudortacal@gmail.com>
2017-11-04 18:43:53 +0000

    feature: Create BookShelf component which renders div

---
+++ src/BookShelf.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import * as BooksAPI from './BooksAPI'
+import "../public/App.css";
+
+export default function BookShelf() {
+  return (
+    <div className="bookshelf">
+    </div>
+  );
+}
