bcbd45b138bd8a987f903c0bb79b42d1acd57e0c
Tudor Tacal <tudortacal@gmail.com>
2017-11-04 17:59:40 +0000

    feature: BooksList renders a Header component
    - everything renders correclty on page

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,13 +1,10 @@
 import React from 'react'
+import Header from './Header'
 // import * as BooksAPI from './BooksAPI'
 import '../public/App.css'
 
 export default function BooksList() {
     return (
-        <div className='list-books'>
-            <div className='list-books-title'>
-                <h1>My Reads</h1>
-            </div>
-        </div>
+        <Header/>
     )
 }
