7400badf8e4a0d7e8325dbe987c0a0e2e0fdb536
Tudor Tacal <tudortacal@gmail.com>
2017-11-04 16:36:08 +0000

    test: Test MyReads title is rendered by BooksList

--- src/tests/BooksList.test.js
+++ src/tests/BooksList.test.js
@@ -10,11 +10,21 @@ describe("BooksList", () => {
     })
 
     it("renders a div with the class 'list-books'", () => {
-        expect(wrapper.find('.list-books')).toBeDefined();
+        expect(wrapper.find(BooksList).filter('list-books')).toBeDefined();
     });
 
-     it("renders a div with the class 'list-books-title'", () => {
+    it("renders a div with the class 'list-books-title'", () => {
         expect(wrapper.find('.list-books-title')).toBeDefined();
     });
+
+    it ("renders a title with the text 'My Reads'", () => {
+        expect(wrapper.contains(
+            <div className='.list-books'>
+                <div className='list-books-title'>
+                    <h1>My Reads</h1>
+                </div>
+            </div>
+        )).toBe(true);
+    })
 
 })
