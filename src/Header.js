e724a6441d45d692fc5244972980f3de76f7ebcf
Tudor Tacal <tudortacal@gmail.com>
2017-11-04 17:55:58 +0000

    feature: Create Header component to render the title

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import '../public/App.css'
+
+export default function Header() {
+    return (
+        <div className='list-books'>
+            <div className='list-books-title'>
+                <h1>My Reads</h1>
+            </div>
+        </div>
+    )
+}
