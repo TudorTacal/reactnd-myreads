0f5fff5ef791e55b136286ed2d333e46114cda90
Tudor Tacal <tudortacal@gmail.com>
2017-11-05 10:43:37 +0000

    test: BooksList sets state on mount with mocked data
    - TODO: make the test work with a mocked BooksAPI module
    and mocked books

--- src/tests/BooksList.test.js
+++ src/tests/BooksList.test.js
@@ -1,9 +1,14 @@
 import { shallow, mount } from "enzyme";
 import React from "react";
 import BooksList from "../BooksList";
-import BookShelf from "../BookShelf"
+import BookShelf from "../BookShelf";
 import Header from "../Header";
+import books from "./MockedBooks";
+import * as BooksAPI from "../BooksAPI";
 import "./testSetup";
+
+jest.mock("../BooksAPI");
+
 
 describe("BooksList", () => {
   let wrapper;
@@ -14,8 +19,8 @@ describe("BooksList", () => {
       title1: "Currently Reading",
       title2: "Want to Read",
       title3: "Read"
-    }
-    wrapper = shallow(<BooksList />);
+    };
+    wrapper = shallow(<BooksList />, { lifecycleExperimental: true });
   });
 
   it("renders a div with the class 'list-books-content'", () => {
@@ -23,14 +28,16 @@ describe("BooksList", () => {
   });
 
   it("renders three BookShelf components with titles", () => {
-    expect(wrapper.contains(<BookShelf title={props.title1}/>)).toBe(true);
-    expect(wrapper.contains(<BookShelf title={props.title2}/>)).toBe(true);
-    expect(wrapper.contains(<BookShelf title={props.title3}/>)).toBe(true);
-  })
-
-  describe("componentDidMount", () => {
-      it("fetches a list of books from the BooksAPI and saves it to its state", () => {
-        
-      })
-  })
+    expect(wrapper.contains(<BookShelf title={props.title1} />)).toBe(true);
+    expect(wrapper.contains(<BookShelf title={props.title2} />)).toBe(true);
+    expect(wrapper.contains(<BookShelf title={props.title3} />)).toBe(true);
+  });
+//   TODO
+//   describe("componentDidMount", () => {
+//     it("fetches a list of books from the BooksAPI and saves it to its state", () => {
+//       return BooksAPI.getAll().then(() => {
+//         expect(wrapper.state("books")).toEqual(books);
+//       });
+//     });
+//   });
 });
