53c47f90bcc3df2fe663bd07457be577450ab4c4
Tudor Tacal <tudortacal@gmail.com>
2017-11-05 19:43:39 +0000

    feature: Book renders divs 'book-cover' & 'book-shelf-changer'

--- src/Book.js
+++ src/Book.js
@@ -5,6 +5,11 @@ class Book extends React.Component {
     render () {
         return (
             <div className="book">
+                <div className="book-top">
+                    <div className="book-cover"/>
+                    <div className="book-shelf-changer">
+                 </div>
+                </div>
                 <div className="book-title"></div>
                 <div className="book-authors"></div>
             </div>
--- src/tests/Book.test.js
+++ src/tests/Book.test.js
@@ -8,18 +8,30 @@ describe("Book", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Book/>);
+    wrapper = shallow(<Book />);
   });
 
-  it("renders a div with a className 'book'",() => {
-      expect(wrapper.find(".book").exists()).toBe(true);
-  })
+  it("renders a div with a className 'book'", () => {
+    expect(wrapper.find(".book").exists()).toBe(true);
+  });
 
-  it("renders a div with a className 'book-title'",() => {
-      expect(wrapper.contains(<div className="book-title"></div>)).toBe(true);
-  })
+  it("renders a div with a className 'book-title'", () => {
+    expect(wrapper.contains(<div className="book-title" />)).toBe(true);
+  });
 
-  it("renders a div with a className 'book-authors'",() => {
-      expect(wrapper.contains(<div className="book-authors"></div>)).toBe(true);
-  })
+  it("renders a div with a className 'book-authors'", () => {
+    expect(wrapper.contains(<div className="book-authors" />)).toBe(true);
+  });
+
+  it("renders a div with a className 'book-top'", () => {
+    expect(wrapper.find(".book-top").exists()).toBe(true);
+  });
+
+  it("renders a div with a className 'book-cover'", () => {
+    expect(wrapper.find(".book-cover").exists()).toBe(true);
+  });
+
+  it("renders a div with a className 'book-shelf-changer'", () => {
+    expect(wrapper.find(".book-shelf-changer").exists()).toBe(true);
+  });
 });
