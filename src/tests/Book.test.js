778f828e7e1f85d83a21fd264140164309cbfdd7
Tudor Tacal <tudortacal@gmail.com>
2017-11-05 19:08:38 +0000

    test: Test Book component renders div class 'book'

---
+++ src/tests/Book.test.js
@@ -0,0 +1,17 @@
+import { shallow, mount } from "enzyme";
+import React from "react";
+import Book from "../Book";
+import books from "./MockedBooks";
+import "./testSetup";
+
+describe("Book", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Book />);
+  });
+
+  it("renders a div with a className 'book'",() => {
+      expect(wrapper.find('book').exists()).toBe(true);
+  })
+});
