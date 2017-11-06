18b9e873496162547c55ffa18ae882c5c2ffa963
Tudor Tacal <tudortacal@gmail.com>
2017-11-06 17:58:15 +0000

    feature: BookShelf renders div class 'bookshelf-books'

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -6,6 +6,8 @@ export default function BookShelf(props) {
   return (
     <div className="bookshelf">
         <h2 className="bookshelf-title">{props.title}</h2>
+        <div className="bookshelf-books">
+        </div>
     </div>
   );
 }
