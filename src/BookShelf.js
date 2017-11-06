56f39c0bc4069f5e3bfbe38ad95fd1bb64516f9f
Tudor Tacal <tudortacal@gmail.com>
2017-11-06 23:14:18 +0000

    feature: BookShelf renders <ol> with Books from props
    test: BookShelf renders <ol> with Books from props

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,15 +1,27 @@
 import React from "react";
 // import * as BooksAPI from './BooksAPI'
+import Book from "./Book";
 import "../public/App.css";
 
-export default function BookShelf(props) {
-  return (
+class BookShelf extends React.Component {
+  render() {
+    return (
     <div className="bookshelf">
-        <h2 className="bookshelf-title">{props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-          </ol>
-        </div>
+      <h2 className="bookshelf-title">
+        {this.props.title}
+      </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {this.props.books.map((book) => 
+            <li key={book.id}>
+              <Book {...book} />
+            </li>
+          )}
+        </ol>
+      </div>
     </div>
   );
+  }
 }
+
+export default BookShelf
--- src/tests/BookShelf.test.js
+++ src/tests/BookShelf.test.js
@@ -1,13 +1,43 @@
 import { shallow } from "enzyme";
 import React from "react";
+import Book from "../Book";
 import BookShelf from "../BookShelf";
 import "./testSetup";
 
 describe("BookShelf", () => {
   let wrapper;
   let title = "Currently Reading";
+  let book1, book2;
+  let props;
   beforeEach(() => {
-    wrapper = shallow(<BookShelf title={title} />);
+    props = {
+      title,
+      books: [
+        {
+          title: "The Linux Command Line",
+          authors: ["William E. Shotts Jr."],
+          imageLinks: {
+            smallThumbnail:
+              "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+            thumbnail:
+              "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+          },
+          id: "nggnmAEACAAJ"
+        },
+        {
+          title: "Learning Web Development with React and Bootstrap",
+          authors: ["Harmeet Singh", "Mehul Bhatt"],
+          imageLinks: {
+            smallThumbnail:
+              "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+            thumbnail:
+              "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+          },
+          id: "sJf1vQAACAAJ"
+        }
+      ]
+    };
+    wrapper = shallow(<BookShelf {...props}  />); 
   });
 
   it("renders a div with the class 'list-books'", () => {
@@ -20,15 +50,26 @@ describe("BookShelf", () => {
 
   it("renders a title which comes from props", () => {
     expect(wrapper.find(".bookshelf-title").text()).toBe("Currently Reading");
-  })
+  });
 
-   it("renders a div with the class 'bookshelf-books'", () => {
+  it("renders a div with the class 'bookshelf-books'", () => {
     expect(wrapper.find(".bookshelf-books").exists()).toBe(true);
   });
 
-   it("renders an ol with the class 'books-grid'", () => {
-    expect(wrapper.find(".books-grid").type()).toBe('ol');
+  it("renders an ol with the class 'books-grid'", () => {
+    expect(wrapper.find(".books-grid").type()).toBe("ol");
   });
 
-
+  it("renders an ol with books components from props", () => {
+      expect(wrapper.find(".books-grid").contains(
+        <ol className="books-grid">
+          <li>
+            <Book {...props.books[0]} />
+          </li>
+          <li>
+            <Book {...props.books[1]}/>
+          </li>
+        </ol>
+      )).toBe(true);
+  });
 });
