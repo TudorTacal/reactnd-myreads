b8558a53f316a9277c8ef7ea44856bc48ae24a3b
Tudor Tacal <tudortacal@gmail.com>
2017-11-08 21:35:28 +0000

    feature: Render Books default select option value according to the shelf it belongs to

--- src/Book.js
+++ src/Book.js
@@ -8,11 +8,11 @@ class Book extends React.Component {
         <div className="book-top">
           <div className="book-cover" style={{ backgroundImage: `url(${this.props.imageLinks.smallThumbnail})` }} />
           <div className="book-shelf-changer">
-            <select>
+            <select defaultValue={this.props.shelf}>
               <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option> 
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
+              <option value="currentlyReading" >Currently Reading</option> 
+              <option value="wantToRead" >Want to Read</option>
+              <option value="read" >Read</option>
               <option value="none">None</option>
             </select>
           </div>
